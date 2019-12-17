e4ea4d25f62a6c53ec4757bf454ec9ce6c1b2290
Gregory Smith <gregorysmith@salesforce.com>
2019-12-16 17:11:31 -0700

    Add an output channel for templates extension
    to help with debugging errors and perf later.
    It's currently controlled by an adx-templates.logging.enabled configuration
    flag, which currently defauls to false, but users can set it to true in the
    Settings.

--- extensions/analyticsdx-vscode-templates/src/index.ts
+++ extensions/analyticsdx-vscode-templates/src/index.ts
@@ -9,13 +9,15 @@ import * as vscode from 'vscode';
 import { telemetryService } from './telemetry';
 import { TemplateEditingManager } from './templateEditing';
 import { TemplateLinterManager } from './templateLinter';
+import { Logger } from './util/logger';
 import { uriDirname } from './util/vscodeUtils';
 
 export type ExtensionType =
-  | {
+  | Readonly<{
       templateEditingManager: TemplateEditingManager;
       templateLinterManager: TemplateLinterManager;
-    }
+      logger: Logger;
+    }>
   | undefined;
 
 export function activate(context: vscode.ExtensionContext): ExtensionType {
@@ -27,23 +29,41 @@ export function activate(context: vscode.ExtensionContext): ExtensionType {
     return;
   }
 
-  const templateEditingManager = new TemplateEditingManager(context);
+  const config = vscode.workspace.getConfiguration('adx-templates');
+  const output = config.get<boolean>('logging.enabled', false)
+    ? vscode.window.createOutputChannel('Analytics DX Templates')
+    : undefined;
+  if (output) {
+    context.subscriptions.push(output);
+  }
+  const logger = new Logger(output, {
+    // these 2 are intentionally not available in the Settings UI and are really just for when we run tests
+    // TODO: default this one to true if we're running from devmode (F5), otherwise false
+    toConsole: config.get<boolean>('logging.console', true),
+    prefix: config.get<string>('logging.prefix')
+  });
+
+  const templateEditingManager = new TemplateEditingManager(context, logger);
   const templateLinterManager = new TemplateLinterManager((doc, tree) => {
     const dir = uriDirname(doc.uri);
     templateEditingManager.setParsedTemplateInfo(dir, tree);
-  });
+  }, logger);
 
   context.subscriptions.push(templateEditingManager.start());
   context.subscriptions.push(templateLinterManager.start());
 
-  console.log('Analytics DX Templates Extension Activated');
+  logger.log(
+    'Analytics DX Templates Extension Activated on ' +
+      vscode.workspace.workspaceFolders.map(f => f.uri.toString()).join(', ')
+  );
   // Notify telemetry that our extension is now active
   telemetryService.sendExtensionActivationEvent(extensionHRStart).catch();
 
-  return {
+  return Object.freeze({
     templateEditingManager,
-    templateLinterManager
-  };
+    templateLinterManager,
+    logger
+  });
 }
 
 export function deactivate() {
