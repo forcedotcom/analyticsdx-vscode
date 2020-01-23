c51aa5d51150119b706a1056679e6b79f0260801
Gregory Smith <gregorysmith@salesforce.com>
2020-01-23 14:18:17 -0700

    See if forcing --no-sandbox helps
    It shouldn't since vscode-test@1.3.0 is already doing it, but you never know.

--- scripts/run-vscode-integration-tests.js
+++ scripts/run-vscode-integration-tests.js
@@ -23,7 +23,8 @@ const { downloadDirToExecutablePath, insidersDownloadDirToExecutablePath } = req
       // workspace path
       path.join(__dirname, '..', 'test-assets', 'sfdx-simple'),
       // turn off any user extensions, so it should run with only extensions setup in the run target
-      '--disable-extensions'
+      '--disable-extensions',
+      '--no-sandbox'
     ]
   };
   console.log('Running vscode tests with:\n', JSON.stringify(opts, undefined, 2));
