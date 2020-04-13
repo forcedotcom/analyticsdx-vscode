3165c8095ef00565fd665cb4a43a02f18c012ed1
Greg Smith <smithgp@users.noreply.github.com>
2020-04-13 15:48:40 -0600

    Update to Open Studio commands (#83)
    * Switch to use /wave/wave.app to open analytic studio.
    This also adds an optional configuration if the user needs to change it to
    something else, but all orgs should be defaulting to /wave/wave.app already.
    
    * Add open data manager command
    
    * Use seperate telemetry logNames for commands that open studio.
    This way we can differentiate them in appinsights.
    
    * Make test more exact on expcted diagnostics
    to try to fix it on windows in Github CI, where it sees 0 diagnostics when
    there's supposed to be 1, which is probably (hopefully) a timing issue between
    the diagnostics getting cleared at somepoint and then set to the real errors.
    Also, fix some expect descriptions.

--- extensions/analyticsdx-vscode-core/src/commands/openStudio.ts
+++ extensions/analyticsdx-vscode-core/src/commands/openStudio.ts
@@ -5,7 +5,7 @@
  * For full license text, see LICENSE.txt file in the repo root or https://opensource.org/licenses/BSD-3-Clause
  */
 
-import { URI_PATHS } from '../constants';
+import * as vscode from 'vscode';
 import { nls } from '../messages';
 import {
   emptyParametersGatherer,
@@ -16,16 +16,30 @@ import {
 } from './commands';
 import { AppGatherer, AppMetadata } from './gatherers/appGatherer';
 
+export function baseStudioPath() {
+  return (
+    vscode.workspace
+      .getConfiguration()
+      .get<string>('adx-core.studio.path')
+      ?.trimLeft() || '/wave/wave.app'
+  );
+}
+
 // REVIEWME: just get the url from sfdx and open in a vscode WebViewPanel?
 class OpenStudioExecutor<T> extends SfdxCommandletExecutor<T> {
-  constructor(private readonly hashgen?: (t: T) => string | undefined) {
+  constructor(
+    private readonly logName = 'analytics_open_studio',
+    private readonly hashgen?: (t: T) => string | undefined
+  ) {
     super();
   }
 
   public build(data: T) {
-    let path = URI_PATHS.Studio;
+    let path = baseStudioPath();
     const hash = this.hashgen ? this.hashgen(data) : undefined;
     if (hash) {
+      // if they put a #hash in the path in the config, chop that off to put this hash on
+      path = path.replace(/#.*$/, '');
       path += '#' + hash;
     }
     return new SfdxCommandBuilder()
@@ -33,7 +47,7 @@ class OpenStudioExecutor<T> extends SfdxCommandletExecutor<T> {
       .withArg('force:org:open')
       .withArg('-p')
       .withArg(path)
-      .withLogName('analytics_open_studio')
+      .withLogName(this.logName)
       .build();
   }
 }
@@ -48,10 +62,21 @@ export async function openStudio() {
   await openStudioCommandlet.run();
 }
 
+const openDataManagerCommandlet = new SfdxCommandlet(
+  sfdxWorkspaceChecker,
+  emptyParametersGatherer,
+  new OpenStudioExecutor('analytics_open_dataManager', () => 'dataManager')
+);
+
+export async function openDataManager() {
+  await openDataManagerCommandlet.run();
+}
+
 const openAppCommandlet = new SfdxCommandlet(
   sfdxWorkspaceChecker,
   new AppGatherer(),
   new OpenStudioExecutor<AppMetadata>(
+    'analytics_open_app_in_studio',
     app => 'application/' + encodeURIComponent(app.folderid) + '/edit'
   )
 );
