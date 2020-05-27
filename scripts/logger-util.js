55caaeec7c059d3698c2d7a092713a57fd162d48
Gregory Smith <gregorysmith@salesforce.com>
2020-05-27 10:19:06 -0600

    Add license header on build scripts

--- scripts/logger-util.js
+++ scripts/logger-util.js
@@ -1,11 +1,17 @@
-const RESET = "\x1b[0m";
-const BOLD = "\x1b[1m";
+/*
+ * Copyright (c) 2020, salesforce.com, inc.
+ * All rights reserved.
+ * Licensed under the BSD 3-Clause license.
+ * For full license text, see LICENSE.txt file in the repo root or https://opensource.org/licenses/BSD-3-Clause
+ */
+
+const RESET = '\x1b[0m';
+const BOLD = '\x1b[1m';
 const CYAN_BOLD = `${BOLD}\x1b[36m%s${RESET}`;
 const RED = `\x1b[31m%s${RESET}`;
 const YELLOW = `\x1b[33m%s${RESET}`;
 
 module.exports = {
-
   header: str => {
     console.log(CYAN_BOLD, str);
   },
