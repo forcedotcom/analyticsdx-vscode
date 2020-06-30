15efc7bb18c19268d2f64aa2c024611c243f2b2d
Gregory Smith <gregorysmith@salesforce.com>
2020-06-30 17:55:08 -0600

    Fix test:vscode-insiders-integration
    So it works on all platforms

--- scripts/download-vscode-for-tests.js
+++ scripts/download-vscode-for-tests.js
@@ -26,11 +26,10 @@ function symlink(src, dest) {
 // Downloads an instance of VS Code for tests to either .vscode-test/insiders or .vscode-test/stable
 (async function() {
   const installDir = process.env.CODE_VERSION === 'insiders' ? 'insiders' : 'stable';
-  console.log(`#!# Downloading ${installDir}...`);
   const electron = await downloadAndUnzipVSCode(installDir);
   // downloadAndUnzipVSCode generates a path (with a version #) to the Electron executable, like:
-  //   .../.vscode-test/vscode-1.36.1/Visual Studio Code.app/Contents/MacOS/Electron
-  // but our stuff is expecting a .../.vscode-test/stable for stable version, so symlink
+  //   .../.vscode-test/vscode-1.36.1/Visual Studio Code.app/Contents/...
+  // but our stuff is expecting a .../.vscode-test/stable/... for stable version, so symlink
   const vscodeTestDir = path.join(process.cwd(), '.vscode-test');
   // this should give us the top install dir path part of vscode (e.g. 'vscode-1.36.1')
   const reldir = path.relative(vscodeTestDir, electron).split(path.sep)[0];
