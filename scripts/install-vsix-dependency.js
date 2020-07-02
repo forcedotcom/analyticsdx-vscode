a2bfeca2ecee2ee944d4af2f9132a7a0d106183e
Greg Smith <smithgp@users.noreply.github.com>
2020-07-02 11:34:53 -0600

    Fix json language server in vscode 1.47+ (#98)
    * Use vscode-json-languageserver module
    
    Instead of using the one from the vscode.json-language-features extension in the
    vscode install. In insiders 1.47, that server code in the extension changed
    enough that our extension wasn't able to start it up anymore.
    This gives us a known version of the json language server to work with so we're
    independent from the vscode (and lsp) version.
    
    * Fix test:vscode-insiders-integration
    
    So it works on all platforms, and hook it into our github actions builds so we know earlier if the extension are going to fail in the next version of vscode.

--- scripts/install-vsix-dependency.js
+++ scripts/install-vsix-dependency.js
@@ -13,8 +13,7 @@ const fs = require('fs');
 const mkdirp = require('mkdirp');
 
 // Installs a list of extensions passed on the command line
-var version = process.env.CODE_VERSION || '*';
-var isInsiders = version === 'insiders';
+var isInsiders = process.env.CODE_VERSION === 'insiders';
 
 const testRunFolder = path.join('.vscode-test', isInsiders ? 'insiders' : 'stable');
 const testRunFolderAbsolute = path.join(process.cwd(), testRunFolder);
@@ -22,17 +21,22 @@ const testRunFolderAbsolute = path.join(process.cwd(), testRunFolder);
 const downloadPlatform =
   process.platform === 'darwin' ? 'darwin' : process.platform === 'win32' ? 'win32-archive' : 'linux-x64';
 
-const windowsExecutable = path.join(testRunFolderAbsolute, 'bin', 'code');
+const windowsExecutable = path.join(testRunFolderAbsolute, 'bin', isInsiders ? 'code-insiders' : 'code');
 const darwinExecutable = path.join(
   testRunFolderAbsolute,
-  'Visual Studio Code.app',
+  isInsiders ? 'Visual Studio Code - Insiders.app' : 'Visual Studio Code.app',
   'Contents',
   'Resources',
   'app',
   'bin',
   'code'
 );
-const linuxExecutable = path.join(testRunFolderAbsolute, 'VSCode-linux-x64', 'bin', 'code');
+const linuxExecutable = path.join(
+  testRunFolderAbsolute,
+  'VSCode-linux-x64',
+  'bin',
+  isInsiders ? 'code-insiders' : 'code'
+);
 
 const extensionsDir = path.join('.vscode-test', 'extensions');
 if (!fs.existsSync(extensionsDir)) {
@@ -44,7 +48,7 @@ const executable =
 
 if (process.platform === 'linux') {
   // Somehow the code executable doesn't have +x set on the autobuilds -- set it here
-  shell.chmod('+x', `${executable}`);
+  shell.chmod('+x', executable);
 }
 
 // We always invoke this script with 'node install-vsix-dependency arg ...'
