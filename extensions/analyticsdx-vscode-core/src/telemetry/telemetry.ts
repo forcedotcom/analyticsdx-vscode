208a7a6ad115f45553909b4acd02857aa701b797
Gregory Smith <gregorysmith@salesforce.com>
2021-10-15 20:47:56 -0600

    Include extensionMode in telemetry check
    Only do telemetry when extension is running in Production mode.

--- extensions/analyticsdx-vscode-core/src/telemetry/telemetry.ts
+++ extensions/analyticsdx-vscode-core/src/telemetry/telemetry.ts
@@ -9,14 +9,41 @@ import { TelemetryService as BaseTelemetryService } from '@salesforce/salesforce
 import * as vscode from 'vscode';
 import { EXTENSION_NAME } from '../constants';
 
+class VscodeBaseTelemetryService extends BaseTelemetryService {
+  private extensionMode: vscode.ExtensionMode | undefined = undefined;
+
+  public override initializeService(
+    context: vscode.ExtensionContext,
+    extensionName: string,
+    aiKey: string,
+    version: string
+  ): Promise<void> {
+    this.extensionMode = context.extensionMode;
+    return super.initializeService(context, extensionName, aiKey, version);
+  }
+
+  public override isTelemetryEnabled(): Promise<boolean> {
+    if (this.extensionMode === vscode.ExtensionMode.Production) {
+      return super.isTelemetryEnabled();
+    }
+    return Promise.resolve(false);
+  }
+}
+
 export class TelemetryService {
   private static instance: TelemetryService;
 
-  constructor(private readonly delegate: BaseTelemetryService = new BaseTelemetryService()) {}
+  constructor(private readonly delegate: BaseTelemetryService = new VscodeBaseTelemetryService()) {}
 
-  /** Get the singleton instance of this, tied to the default singleton of the underlying telemetry service */
+  /** Get the singleton instance of this, and setup to the default singleton of the underlying telemetry service,
+   * which is used by the cli command base code.
+   */
   public static getInstance() {
     if (!TelemetryService.instance) {
+      if (!((BaseTelemetryService as any).instance instanceof VscodeBaseTelemetryService)) {
+        const base = new VscodeBaseTelemetryService();
+        (BaseTelemetryService as any).instance = base;
+      }
       TelemetryService.instance = new TelemetryService(BaseTelemetryService.getInstance());
     }
     return TelemetryService.instance;
