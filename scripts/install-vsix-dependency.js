6dc4a89759c3573094f50a602f0140d80a431c57
Gregory Smith <gregorysmith@salesforce.com>
2023-03-07 11:24:55 -0700

    Update @vscode/test-electron version

--- scripts/install-vsix-dependency.js
+++ scripts/install-vsix-dependency.js
@@ -17,34 +17,35 @@ var isInsiders = process.env.CODE_VERSION === 'insiders';
 const testRunFolder = path.join('.vscode-test', isInsiders ? 'insiders' : 'stable');
 const testRunFolderAbsolute = path.join(process.cwd(), testRunFolder);
 
-const windowsExecutable = path.join(testRunFolderAbsolute, 'bin', isInsiders ? 'code-insiders' : 'code');
-const darwinExecutable = path.join(
-  testRunFolderAbsolute,
-  isInsiders ? 'Visual Studio Code - Insiders.app' : 'Visual Studio Code.app',
-  'Contents',
-  'Resources',
-  'app',
-  'bin',
-  'code'
-);
-const linuxExecutable = path.join(
-  testRunFolderAbsolute,
-  'VSCode-linux-x64',
-  'bin',
-  isInsiders ? 'code-insiders' : 'code'
-);
+let executable;
+if (process.platform === 'darwin') {
+  executable = path.join(
+    testRunFolderAbsolute,
+    isInsiders ? 'Visual Studio Code - Insiders.app' : 'Visual Studio Code.app',
+    'Contents',
+    'Resources',
+    'app',
+    'bin',
+    'code'
+  );
+} else if (process.platform === 'win32') {
+  executable = path.join(testRunFolderAbsolute, 'bin', isInsiders ? 'code-insiders' : 'code');
+} else {
+  // assume linux
+  executable = testRunFolderAbsolute;
+  // downloadAndUnzipVSCode() in download-vscode-for-tests.js used to leave this folder under testRunFolder, but
+  // now doesn't seem to to, so handle both cases
+  if (fs.readdirSync(testRunFolderAbsolute).includes(name => name === 'VSCode-linux-x64')) {
+    executable = path.join(executable, 'VSCode-linux-x64');
+  }
+  executable = path.join(executable, 'bin', isInsiders ? 'code-insiders' : 'code');
+  // Sometimes the code executable doesn't have +x set on the github actions -- set it here
+  shell.chmod('+x', executable);
+}
 
 const extensionsDir = path.join('.vscode-test', 'extensions');
 if (!fs.existsSync(extensionsDir)) {
   fs.mkdirSync(extensionsDir, { recursive: true });
-}
-
-const executable =
-  process.platform === 'darwin' ? darwinExecutable : process.platform === 'win32' ? windowsExecutable : linuxExecutable;
-
-if (process.platform === 'linux') {
-  // Somehow the code executable doesn't have +x set on the autobuilds -- set it here
-  shell.chmod('+x', executable);
 }
 
 // We always invoke this script with 'node install-vsix-dependency arg ...'
